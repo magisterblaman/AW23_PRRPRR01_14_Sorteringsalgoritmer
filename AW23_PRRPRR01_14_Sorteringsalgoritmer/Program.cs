39cbe3ddf283e890b5b5deb2fca1e6429ddb9aa4
magisterblaman <hannes.blaman@lbs.se>
2024-11-21 13:09:53 +0100

    Fixad bubble sort

--- AW23_PRRPRR01_14_Sorteringsalgoritmer/Program.cs
+++ AW23_PRRPRR01_14_Sorteringsalgoritmer/Program.cs
@@ -20,7 +20,7 @@ namespace AW23_PRRPRR01_14_Sorteringsalgoritmer {
 			for (int j = inputArray.Length; j > 0; j--) {
 				for (int i = 1; i < j; i++) {
 					if (inputArray[i] < inputArray[i - 1]) {
-						int temp = inputArray[i];
+						int temp = inputArray[i - 1];
 						inputArray[i - 1] = inputArray[i];
 						inputArray[i] = temp;
 					}
@@ -66,16 +66,24 @@ namespace AW23_PRRPRR01_14_Sorteringsalgoritmer {
 		static void Main(string[] args) {
 			int[] myArray = new int[] { 7, 5, -3, 1, 5465, 76, 4, 3, -3 };
 
+			for (int i = 0; i < myArray.Length; i++) {
+				Console.WriteLine(myArray[i]);
+			}
 			Console.WriteLine(IsSorted(myArray));
 
-			InsertionSort(myArray);
+			Console.WriteLine("====");
 
+			BubbleSort(myArray);
+
+			for (int i = 0; i < myArray.Length; i++) {
+				Console.WriteLine(myArray[i]);
+			}
 			Console.WriteLine(IsSorted(myArray));
 
 			//for (int j = myArray.Length; j > 0; j--) {
 			//	for (int i = 1; i < j; i++) {
 			//		if (myArray[i] < myArray[i - 1]) {
-			//			int temp = myArray[i];
+			//			int temp = myArray[i - 1];
 			//			myArray[i - 1] = myArray[i];
 			//			myArray[i] = temp;
 			//		}
@@ -84,7 +92,7 @@ namespace AW23_PRRPRR01_14_Sorteringsalgoritmer {
 
 			//for (int i = 1; i < myArray.Length - 0; i++) {
 			//	if (myArray[i] < myArray[i - 1]) {
-			//		int temp = myArray[i];
+			//		int temp = myArray[i - 1];
 			//		myArray[i - 1] = myArray[i];
 			//		myArray[i] = temp;
 			//	}
@@ -92,7 +100,7 @@ namespace AW23_PRRPRR01_14_Sorteringsalgoritmer {
 
 			//for (int i = 1; i < myArray.Length - 1; i++) {
 			//	if (myArray[i] < myArray[i - 1]) {
-			//		int temp = myArray[i];
+			//		int temp = myArray[i - 1];
 			//		myArray[i - 1] = myArray[i];
 			//		myArray[i] = temp;
 			//	}
@@ -100,7 +108,7 @@ namespace AW23_PRRPRR01_14_Sorteringsalgoritmer {
 
 			//for (int i = 1; i < myArray.Length - 2; i++) {
 			//	if (myArray[i] < myArray[i - 1]) {
-			//		int temp = myArray[i];
+			//		int temp = myArray[i - 1];
 			//		myArray[i - 1] = myArray[i];
 			//		myArray[i] = temp;
 			//	}
